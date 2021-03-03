28ad6a356b35ef486bf4406877b7a05e5c32a78c
joelwsaury <saury.joel@gmail.com>
2021-03-03 13:24:32 +0100

    print parameter filter content

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -19,15 +19,16 @@
                         </div>
                     </li>
                     <li class="nav-item" v-for="parameter in getFilterableParameterDefinitions()" :key="parameter.id">
-                        <a class="nav-link justify-content-between d-flex" data-bs-toggle="collapse" v-bind:href="parameter.name+'List'" role="button" aria-expanded="false" v-bind:aria-controls="parameter.name + 'List'"> 
+                        <a class="nav-link justify-content-between d-flex" data-bs-toggle="collapse" v-bind:href="'#'+parameter.name+'List'" role="button" aria-expanded="false" v-bind:aria-controls="parameter.name + 'List'"> 
                         {{parameter.name}} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down pull-right" viewBox="0 0 16 16">
                             <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
                         </svg> 
                         </a>
                         <div class="collapse multi-collapse" v-bind:id="parameter.name+'List'">
                             <div class="card card-body">
-                                <ul class="list-group vertical-scroll">
-                                </ul>
+                                   <li class="list-group-item" v-for="str in groupByParameter(parameter)" :key="str">
+                                         <input type="checkbox"> {{str}}
+                                    </li>
                             </div>
                         </div>
                     </li>
@@ -36,6 +37,10 @@
 </template>
 
 <script lang="ts">
+// eslint-disable-next-line no-unused-vars
+import { ParamDefinition } from '@/core/Parameter';
+// eslint-disable-next-line no-unused-vars
+import { TimeLineBase } from '@/core/TimeLineBase';
 import { Options, Vue } from 'vue-class-component';
 // eslint-disable-next-line no-unused-vars
 import {Session} from '../core/Session';
@@ -53,8 +58,31 @@ export default class SideMenu extends Vue {
 getFilterableParameterDefinitions() {
    return this.session.parameterDefs.filter(pDef => pDef.filterable==true);
 }
+
+ groupByParameter(definition: ParamDefinition) : Array<string>{
+     
+let groups : Record<string,TimeLineBase[]> = this.session.timeSpans.groupBy(tSpan => tSpan.getParameter(definition.name).asString());
+return Object.keys(groups);
+}
     
 }
+
+declare global {
+    interface Array<T> {
+        groupBy<T, K extends keyof any>(this:T[], getKey: (item: T) => K): Record<K, T[]>;
+    }
+}
+
+Array.prototype.groupBy = function<T, K extends keyof any>(this:T[], getKey: (item:T) => K){
+
+    return this.reduce( (prev, current) => {
+        const group = getKey(current);
+        if (!prev[group]) prev[group] = [];
+        prev[group].push(current);
+        return prev;
+    }, {} as Record<K,T[]>);
+};
+
 </script>
 
 <style scoped src="../assets/css/app.css"/>
