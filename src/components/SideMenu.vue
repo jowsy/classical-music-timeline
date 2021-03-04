f2a4036984d533bf1dbeccec5b8289afe603310b
joelwsaury <saury.joel@gmail.com>
2021-03-04 20:31:11 +0100

    Implement filtering

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -22,15 +22,18 @@
         <h5 class="justify-content-between mt-4 mb-1 text-muted">Filters</h5>
         <ul class="nav flex-column">
             <li class="nav-item" v-for="parameter in getFilterableParameterDefinitions()" :key="parameter.id">
-                <a class="nav-link justify-content-between d-flex" data-bs-toggle="collapse" v-bind:href="'#'+parameter.name+'List'" role="button" aria-expanded="false" v-bind:aria-controls="parameter.name + 'List'"> 
-                {{parameter.name}} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down pull-right" viewBox="0 0 16 16">
-                    <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                </svg> 
-                </a>
+            <div class="justify-content-between d-flex">
+                <div class="form-check form-switch" data-bs-toggle="collapse" v-bind:href="'#'+parameter.name+'List'" role="button" aria-expanded="false" v-bind:aria-controls="parameter.name + 'List'">         
+                    <input type="checkbox" class="form-check-input" id="customSwitch1" v-on:change="activateFilter($event, parameter.id)"/>
+                    <label class="form-check-label" for="customSwitch1">{{parameter.name}}</label>
+                </div>
+                
+            </div>
+                    
                 <div class="collapse multi-collapse" v-bind:id="parameter.name+'List'">
                     <div class="card card-body">
                             <li class="list-group-item" v-for="str in groupByParameter(parameter)" :key="str">
-                                    <input type="checkbox" v-on:change="applyParameterFilter($event, str, parameter.name)"> {{str}}
+                                    <input type="checkbox" v-on:change="applyParameterFilter($event, parameter.id, parameter.name, str)"> {{str}}
                             </li>
                     </div>
                 </div>
@@ -42,6 +45,8 @@
 <script lang="ts">
 // eslint-disable-next-line no-unused-vars
 import { ParamDefinition } from '@/core/Parameter';
+import { ParameterStringFilter } from '@/core/ParameterFilter';
+import { OrFilter } from '@/core/OrFilter';
 // eslint-disable-next-line no-unused-vars
 import { TimeLineBase } from '@/core/TimeLineBase';
 import { Options, Vue } from 'vue-class-component';
@@ -57,6 +62,7 @@ export default class SideMenu extends Vue {
 
    session : Session;
 
+   filterMap : Map<string,string> = new Map<string,string>();
    //We don't want all parameters to be filterable in the UI
 getFilterableParameterDefinitions() {
    return this.session.parameterDefs.filter(pDef => pDef.filterable==true);
@@ -64,14 +70,40 @@ getFilterableParameterDefinitions() {
 
 groupByParameter(definition: ParamDefinition) : Array<string>{
      
-let groups : Record<string,TimeLineBase[]> = this.session.timeSpans.groupBy(tSpan => tSpan.getParameter(definition.name).asString());
+let groups : Record<string,TimeLineBase[]> = this.session.timeSpans.groupBy(tSpan => tSpan.getParameterByDefinition(definition).asString());
 return Object.keys(groups);
 }
 
-applyParameterFilter(checkBoxCtrl:any, value:string, paramName:string){
-    var timeSpans = this.session.timeSpans.filter(t => t.getParameter(paramName).asString() == value);
+applyParameterFilter(checkBoxCtrl:any, paramDefinitionId:string, paramName:string, value:string){
+    let filterId : string = this.filterMap.get(paramDefinitionId) as string;
     var checked= checkBoxCtrl.target.checked;
-    timeSpans.forEach( t => t.show = checked);
+    var filter = this.session.getFilter(filterId) as OrFilter;
+    console.log("applyParameterFilter, filterId: "+filter.id);
+    if (checked)
+    filter.addFilter(new ParameterStringFilter(paramName, value));
+    else
+    filter.removeFilter()
+    //
+       console.log("Count in OrFilter: "+filter.getCount());
+    this.session.Refresh();
+}
+
+activateFilter(checkBoxCtrl:any, parameterDefId:string){
+
+    var checked= checkBoxCtrl.target.checked;
+
+    if (checked){
+        var filter = new OrFilter();
+        this.filterMap.set(parameterDefId, filter.id)
+        this.session.addFilter(filter);
+           console.log("Add OrFilter, id: "+filter.id);
+    }else{
+        this.filterMap.delete(parameterDefId);
+        this.session.
+     console.log("Delete filter, id: "+parameterDefId);
+    }
+    //var checked= checkBoxCtrl.target.checked;
+    this.session.Refresh();
 }
     
 }
