6ab7e65394db144ece81a200897693e44e56d654
joelwsaury <saury.joel@gmail.com>
2021-03-23 19:56:13 +0100

    New architecture, generate shapes using factory

---
+++ src/core/Composer.ts
@@ -0,0 +1,6 @@
+import { Person } from "./Person";
+
+export class Composer extends Person {
+    /* true if composer has been an important figure from an epoch to another, for example Beethoven (Classical -> Romantic)   */
+    transitional:boolean;
+}
---
+++ src/core/TimeLineShapes.ts
@@ -0,0 +1,11 @@
+export class TimeLineShape {
+    x:number;
+    y:number;
+}
+
+export class TimeLinePoint extends TimeLineShape{
+}
+
+export class TimeLineRectangle extends TimeLineShape {
+    width:number;
+}
