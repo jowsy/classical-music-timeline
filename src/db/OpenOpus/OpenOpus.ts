25c27313fde436b89b47c8a4f88cd5a81f22747a
joelwsaury <saury.joel@gmail.com>
2021-04-17 12:46:25 +0200

    add parameter quantity of works

--- src/db/OpenOpus/OpenOpus.ts
+++ src/db/OpenOpus/OpenOpus.ts
@@ -1,28 +1,38 @@
-export interface Status {
-    version: string;
-    success: string;
-    source: string;
-    rows: number;
-    processingtime: number;
-    api: string;
-}
-export interface Request {
-    type: string;
-    item: string;
-}
-export interface IComposer {
-    id: string;
-    name: string;
-    complete_name: string;
-    birth: string;
-    death: string;
-    epoch: string;
-    portrait: string;
-    popular: number;
-}
-
-export interface RootObject {
-    status: Status;
-    request: Request;
-    composers: IComposer[];
-}
+export interface Root {
+    status: Status
+    request: Request
+    composers: Composer[]
+  }
+  
+  export interface Status {
+    success: string
+    source: string
+    rows: number
+    processingtime: number
+    api: string
+    version: string
+  }
+  
+  export interface Request {
+    type: string
+  }
+  
+  export interface Composer {
+    name: string
+    complete_name: string
+    epoch: string
+    birth: string
+    death: any
+    popular: string
+    recommended: string
+    works: Work[]
+  }
+  
+  export interface Work {
+    title: string
+    subtitle: string
+    searchterms: string
+    popular: string
+    recommended: string
+    genre: string
+  }
--- src/db/OpenOpus/OpenOpusJsonMapper.ts
+++ src/db/OpenOpus/OpenOpusJsonMapper.ts
@@ -1,9 +1,9 @@
 import {IDataGateway} from '../../core/IDataGateway'
 import { ParameterType } from '@/core/Parameter';
-import { RootObject } from './OpenOpus';
+import { Root } from './OpenOpus';
 import { SessionVm } from '@/viewmodel/SessionVm';
 import { TimeLineBase } from '@/core';
-import { ComposerImpl } from './ComposerImpl';
+import * as OpenOpus from './OpenOpus';
 import { Composer } from '@/core/Composer';
 
 /* -------------------------------------------------------
@@ -16,6 +16,7 @@ export class OpenOpusJsonMapper implements IDataGateway {
 
     private epochParameterName : string = "epoch";
     private popParameterName : string = "popularity";
+    private worksParameterName : string = "quantityOfWorks";
 
     constructor(json : string){
         this.json = json;
@@ -29,6 +30,7 @@ export class OpenOpusJsonMapper implements IDataGateway {
     Prepare(): void {
         const epochParameterDef = this.session.configuration.getParameterByName(this.epochParameterName);
         const popParameterDef = this.session.configuration.getParameterByName(this.popParameterName);
+        const worksParameterDef = this.session.configuration.getParameterByName(this.worksParameterName);
         
         if (epochParameterDef == undefined){
             var definition = this.session.configuration.addParameter(this.epochParameterName, "Epoch", ParameterType.String, true);
@@ -38,30 +40,37 @@ export class OpenOpusJsonMapper implements IDataGateway {
         if (popParameterDef == undefined){
             var definition = this.session.configuration.addParameter(this.popParameterName, "Popularity", ParameterType.String, true);
         }
+
+        if (worksParameterDef == undefined){
+            var definition = this.session.configuration.addParameter(this.worksParameterName, "Quantity of Works", ParameterType.Number, true);
+        }
     }
     
     getElements(): TimeLineBase[] {
         let id:number=0;
-        let rootObject : RootObject = JSON.parse(this.json);
+        let rootObject : Root = JSON.parse(this.json);
         let data: Array<TimeLineBase> = new Array<TimeLineBase>();
-        rootObject.composers.forEach(c =>{ 
-            let openOpusComposer : ComposerImpl = Object.assign(new ComposerImpl(), c);
+        (rootObject.composers as OpenOpus.Composer[]).forEach(c =>{ 
+            
             let composer = new Composer();
 
             composer.internalId=id;
             id++;
-        
-            composer.fullName = openOpusComposer.complete_name; 
+            
+            composer.fullName = c.complete_name; 
 
-            composer.birth = new Date(openOpusComposer.birth);
-            if (openOpusComposer.death!=null)
-                composer.death = new Date(openOpusComposer.death);
+            composer.birth = new Date(c.birth);
+            if (c.death!=null)
+                composer.death = new Date(c.death);
             composer.visible = true; //Show by default
 
             composer.session = this.session;
-            composer.getParameterByName(this.epochParameterName).set(openOpusComposer.epoch); 
-            composer.getParameterByName(this.popParameterName).set(openOpusComposer.popular == 1 ? "High": "Low"); 
-            //newTimeSpan.getParameterByName("portrait").set(comp.portrait); 
+            composer.getParameterByName(this.epochParameterName).set(c.epoch); 
+            composer.getParameterByName(this.popParameterName).set(c.popular == "1" ? "High": "Low"); 
+
+            if (c.works.length>0)
+                composer.getParameterByName(this.worksParameterName).set(c.works.length); 
+
 
             data.push(composer);
             });
